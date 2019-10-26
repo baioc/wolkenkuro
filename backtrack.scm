c7e28bf97567e493bd04344f9e996525224aa0a4
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2019-10-25 21:38:58 -0300

    Generalize backtracking with continuation passing

--- backtrack.scm
+++ backtrack.scm
@@ -1,78 +1,61 @@
 ;;;; GENERIC BACKTRACKING USING CONTINUATION-PASSING STYLE
 
-;; success continuation: (lambda (solution) ...)
-;; fail continuation: (lambda () ...)
-;; backtracking procedure: (lambda (problem success fail) ...)
+;; success (return) continuation: (lambda (solution) ...)
+;; fail (backtrack/reject) continuation: (lambda () ...)
+;; elimination (collapse) procedure:
+;;   (lambda (try/else problem fail)
+;;     (try/else something
+;;               (lambda () (try/else otherthing fail))))
 
 ;;; given a nondeterministically solvable problem:
 ;;; if there are still any ambiguities left to resolve:
-;;;   reduce it in a reversible fashion by commiting to some arbitrary computing
-;;;   branch and return the recursive application of this procedure on the
-;;;   reduced problem passing in the same success continuation and a new fail
-;;;   continuation that undoes the previous reduction and removes that from the
-;;;   problem's solution possibility space before invoking another recursion
-;;;   passing in the modified problem and both the original continuations;
-;;; otherwise, if this path led to an invalid solution:
-;;;   call the fail continuation in order to backtrack to a previous state or
-;;;   reject the problem as decidedly unsolvable (original fail continuation);
+;;;   reduce it in a reversible fashion by commiting to some possibly arbitrary
+;;;   computing branch and return the recursive application of this procedure
+;;;   passing in <the reduced problem> and <a new fail continuation> that:
+;;;     undoes the previous reduction and removes that from the solution
+;;;     possibility space before recursing yet again with the newly modified
+;;;     problem and the original fail continuation;
+;;; otherwise, check whether this path led to an invalid solution and if so:
+;;;   call the current fail continuation so as to either backtrack to a previous
+;;;   state and retry from there or reject the problem as decidedly unsolvable;
 ;;; else, the problem has now collapsed to trivially solvable so just:
-;;;   call the success continuation in order to accept that as a solution.
+;;;   call the success continuation in order to accept that solution.
 
-(define (solve solutions succeed fail)
-  (cond ((amb? solutions)
-          (collapse solutions
-                    fail
-                    (lambda (reduced backtrack)
-                      (solve reduced succeed backtrack))))
-        ((not (valid? solutions)) (fail))
-        (else (succeed solutions))))
+(define (solve problem return fail ambiguous? valid? collapse)
+  (let retry ((solutions problem) (backtrack fail))
+    (cond ((ambiguous? solutions)
+            (collapse (lambda (reduced undo) (retry reduced undo))
+                      solutions
+                      backtrack))
+          ((not (valid? solutions)) (backtrack))
+          (else (return solutions)))))
 
 
-;;; example
+;;; a simple example
 
-(define (accept solution)
-  (display "Found it -> ") (display solution) (newline))
+(define search-tree '(((LLL LLR) (LRL LRR)) ((RLL RLR) (RRL RRR))))
 
-(define (reject)
-  (display "Impossible!\n"))
-
-(define (amb? solution)
-  (list? solution))
-
-(define (valid? solution)
-  (eq? solution 'RLR))
-
-(define (collapse problem fail try)
-  (display "trying left... ") (display (car problem)) (newline)
-  (try (car problem)
+(define (tree-prune try root fail)
+  ;; try going left first
+  (display "trying left... ") (display (car root)) (newline)
+  (try (car root)
        (lambda ()
-         (display "trying right... ") (display (cadr problem)) (newline)
-         (try (cadr problem) fail))))
+         ;; when backtracking, go right instead
+         (display "trying right... ") (display (cadr root)) (newline)
+         (try (cadr root) fail))))
 
-(define problem ; decision-tree
-  '(
-    (
-      (
-        LLL
-        LLR
-      )
-      (
-        LRL
-        LRR
-      )
-    )
-    (
-      (
-        RLL
-        RLR
-      )
-      (
-        RRL
-        RRR
-      )
-    )
-  )
+(define (tree-find node)
+  (display "Begin solving ") (display search-tree) (newline)
+  (solve search-tree           ;; original problem
+         (lambda (solution)    ;; success continuation
+           (display "Found it -> ") (display solution) (newline))
+         (lambda ()            ;; fail continuation
+           (display "Impossible!"))
+         (lambda (maybe)       ;; test if there's still work to be done
+           (list? maybe))
+         (lambda (possibility) ;; test if a solution was found
+           (eq? possibility node))
+         tree-prune)           ;; what to do at each decision point
 )
 
-(display "Begin solving ") (display problem) (newline)
-(solve problem accept reject)
+; (tree-find 'RLR)
