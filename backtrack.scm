7b801a0b6c8f921fe0bc5a2d37e3c9c9806372c6
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2019-10-26 15:11:29 -0300

    Refactor 8queens

--- backtrack.scm
+++ backtrack.scm
@@ -3,9 +3,9 @@
 ;; success (return) continuation: (lambda (solution) ...)
 ;; fail (backtrack/reject) continuation: (lambda () ...)
 ;; elimination (collapse) procedure:
-;; (lambda (try/else problem fail)
-;;   (try/else something
-;;             (lambda () (try/else otherthings fail))))
+;;  (lambda (try/else problem fail)
+;;    (try/else something
+;;              (lambda () (try/else otherthings fail))))
 
 ;;; given a nondeterministically solvable problem:
 ;;; if there are still any ambiguities left to resolve:
@@ -54,17 +54,23 @@
            (list? maybe))
          (lambda (possibility) ;; test if a solution was found
            (eq? possibility node))
-         tree-prune)           ;; what to do at each decision point
-)
+         tree-prune))          ;; what to do at each decision point
 
 ; (tree-find 'RLR)
 
 
-;;; Proof Of Concept with the Eight Queens puzzle
+;;; a more robust Proof Of Concept with the Eight Queens puzzle
 
-(load "common.scm") ;; shuffle
+(load "vector.scm") ;; vector-map, vector-find-pos, vector-every-pos
+(load "utilisp.scm") ;; shuffle
 
 (define (eight-queens)
+  ;; check if positions for two queens lead them to a check
+  (define (in-check? xa ya xb yb)
+    (or (= xa xb) ; same row
+        (= ya yb) ; same col
+        (= (abs (- xa xb)) (abs (- ya yb))))) ; same diag
+
   ;; vector containing each of the n queens' possible positions
   (define (queens n)
     (define (board i j lst)
@@ -73,23 +79,13 @@
             (else (board i (- j 1) (cons (cons i j) lst)))))
     (vector-map shuffle (make-vector n (board n n '()))))
 
-  ;; finds the first index of a vector that satisfies a predicate, else #f
-  (define (vector-find pred vec)
-    (let for ((k 0) (n (vector-length vec)))
-      (cond ((= k n) #f)
-            ((pred k) k)
-            (else (for (+ k 1) n)))))
-
-  ;; checks if a vector index satisfies a predicate for every valid index
-  (define (vector-every pred vec)
-    (not (vector-find (lambda (idx) (not (pred idx))) vec)))
-
   ;; check if there are still any ambiguities left to resolve
   (define (amb? board)
-    (vector-find (lambda (q)
-                   (let ((queen (vector-ref board q)))
-                     (and (list? queen) (not (null? queen)))))
-                 board))
+    (vector-find-pos
+      (lambda (q)
+        (let ((queen (vector-ref board q)))
+          (and (list? queen) (not (null? queen)))))
+      board))
 
   ;; check if the board is a solution to the queens puzzle
   (define (valid? positions)
@@ -97,41 +93,36 @@
       (let ((queen (vector-ref positions q)))
         (if (null? queen) #f
             (let ((qx (car queen)) (qy (cdr queen)))
-              (vector-every (lambda (k)
-                              (let ((other (vector-ref positions k)))
-                                (if (null? other) #f
-                                    (or (= k q)
-                                      (let ((x (car other)) (y (cdr other)))
-                                        (and (not (= x qx))
-                                             (not (= y qy))
-                                             (not (= (abs (- x qx))
-                                                     (abs (- y qy))))))))))
-                            positions)))))
-    (vector-every safe? positions))
+              (vector-every-pos
+                (lambda (k)
+                  (let ((other (vector-ref positions k)))
+                    (if (null? other) #f
+                        (or (= k q)
+                            (not (in-check? qx qy (car other) (cdr other)))))))
+                positions)))))
+    (vector-every-pos safe? positions))
 
-  ;; make a guess by collapsing some queen's position
+  ;; make a guess by fixing some queen's position
   (define (emplace try possibilities fail)
     ;; possibility space pruning
     (define (prune board qx qy)
       (vector-map (lambda (others)
-                    (if (list? others)
+                    (if (not (list? others)) others
                         (filter (lambda (position)
-                                  (let ((x (car position)) (y (cdr position)))
-                                    (and (not (= x qx))
-                                         (not (= y qy))
-                                         (not (= (abs (- x qx))
-                                                 (abs (- y qy)))))))
-                                others)
-                        others))
+                                  (not (in-check? qx
+                                                  qy
+                                                  (car position)
+                                                  (cdr position))))
+                                others)))
                   board))
-    (let* ((q (amb? possibilities))
+    (let* ((q (amb? possibilities)) ;; index of first unresolved queen
            (queens (vector-ref possibilities q))
            (queen (car queens))
            (qx (car queen)) (qy (cdr queen)))
-      (vector-set! possibilities q queen)
-      (try (prune possibilities qx qy)
+      (vector-set! possibilities q queen) ;; try this one
+      (try (prune possibilities qx qy) ;; prune others' possibilities
            (lambda ()
-             ;; restore (almost) original possibilities
+             ;; restore original possibilities except for the one from this try
              (vector-set! possibilities q (cdr queens))
              (try possibilities fail)))))
 
